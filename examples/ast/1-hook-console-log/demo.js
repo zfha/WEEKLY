bde1b102ba7175b2d6ac1a12c35ccd3f7ced9e39
zhoufan <zhoufan@qingguatang.com>
2019-03-01 10:35:35 +0800

    :sparkles:

---
+++ examples/ast/1-hook-console-log/demo.js
@@ -0,0 +1,34 @@
+const generator = require('@babel/generator');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse');
+const types = require('@babel/types');
+
+function compile(code) {
+  let ast = parser.parse(code);
+  traverse.default(ast, {
+    CallExpression(path) {
+      let { callee } = path.node;
+      if (
+        types.isMemberExpression(callee) &&
+        callee.object.name === 'console' &&
+        callee.property.name === 'log'
+      ) {
+        let fn = path.findParent(p => {
+          return types.isFunctionDeclaration(p);
+        });
+        path.node.arguments.unshift(
+          types.StringLiteral(`[${fn.node.id.name}]`)
+        );
+      }
+    }
+  });
+  return generator.default(ast).code;
+}
+
+const code = `
+  function hello(){
+    console.log('bmatch');
+  }
+`;
+
+console.log(compile(code));
